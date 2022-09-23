c115cfa4c2c5bda7604f06782e852d614fa0f99c
Mateusz Bogacz <mateusz.bogacz@cyfrowafoto.com>
2022-09-23 14:43:16 +0200

    added a new function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export const add = (first: number, second: number) => first + second;
 export const subtract = (first: number, second: number) => first - second;
 export const multiplication = (first: number, second: number) => first * second;
+
+export const division = (first: number, second: number) => {
+  if (first === 0 || second === 0) {
+    throw Error('we cannot divide by zero');
+  }
+
+  return first / second;
+};
