272536c5d2464ac0864f02b2cb8033b088dafb40
Mateusz Bogacz <mateusz.bogacz@cyfrowafoto.com>
2022-09-23 14:17:25 +0200

    added a new function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export const add = (first: number, second: number) => first + second;
 export const subtract = (first: number, second: number) => first - second;
+export const multiplication = (first: number, second: number) => first * second;
